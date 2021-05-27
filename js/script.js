79b994769e52006e281385ad396e8b7f60596e6e
BlueDokk <katzschwarz@gmail.com>
2021-05-27 08:42:11 -0500

    Define testimonial and footer.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-
-
 var p = {
 
     btnToggler: document.querySelector('.navbar__toggler'),
     navbar: document.querySelector('.navbar'),
+    btnUp: document.getElementById("button-up")
 
 }
 
@@ -11,6 +10,7 @@ var m = {
 
     startApp: () => {
         m.collapse(p.btnToggler, p.navbar);
+        m.scroll();
     },
 
     collapse: (element, target) => {
@@ -18,10 +18,30 @@ var m = {
             target.classList.toggle("collapsible--expanded");
         })
     },
+    scrollUp: () => {
+        var currentScroll = document.documentElement.scrollTop;
+
+        if (currentScroll > 0) {
+            window.requestAnimationFrame(m.scrollUp);
+            window.scrollTo(0, currentScroll - (currentScroll / 10));
+        }
+    },
+    scroll: () => {
+        var scroll = document.documentElement.scrollTop;
+        if (p.btnUp != null) {
+            if (scroll > 100) {
+                p.btnUp.style.transform = 'scale(1)';
+            } else if (scroll < 100) {
+                p.btnUp.style.transform = 'scale(0)';
+            }
+        }
+    }
 }
 
-
 m.startApp();
+window.onscroll = m.scroll;
+
+document.getElementById("button-up").addEventListener("click", m.scrollUp);
 
 
 
